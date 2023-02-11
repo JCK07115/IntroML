1a1025a79659d46eca334f2393f97fdafcca3fea
Chaoyue Xi <chaoyuex@mun.ca>
2023-02-10 21:22:46 -0330

    The code fits the requirement (hope so)

---
+++ assig1/data/assign_1_group_07.py
@@ -0,0 +1,137 @@
+#
+# Assignment 1
+#
+# Group 07
+# Chaoyue Xi chaoyuex@mun.ca
+# <Group Member 2 name> <Group Member 2 email>
+# <Group Member 3 name> <Group Member 3 email>
+
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.neighbors import KNeighborsClassifier
+
+# creating the training datasets
+train_DAT = pd.read_csv('train.sDAT.csv', header=None)
+train_DAT.rename(columns={0: "x1", 1: "x2"}, inplace=True)
+train_DAT['label'] = 1
+
+train_NC = pd.read_csv('train.sNC.csv', header=None)
+train_NC.rename(columns={0: "x1", 1: "x2"}, inplace=True)
+train_NC['label'] = 0
+
+# merging the two training datasets
+train_df = pd.concat([train_DAT, train_NC], axis=0)
+
+# creating the test datasets
+test_DAT = pd.read_csv('test.sDAT.csv', header=None)
+test_DAT.rename(columns={0: "x1", 1: "x2"}, inplace=True)
+test_DAT['label'] = 1
+
+test_NC = pd.read_csv('test.sNC.csv', header=None)
+test_NC.rename(columns={0: "x1", 1: "x2"}, inplace=True)
+test_NC['label'] = 0
+
+# merging the two testing datasets
+test_df = pd.concat([test_DAT, test_NC], axis=0)
+
+grid_points = pd.read_csv('2D_grid_points.csv', header=None)
+grid_points.rename(columns={0: "x1", 1: "x2"}, inplace=True)
+
+k = [1, 3, 5, 10, 20, 30, 50, 100, 150, 200]
+colors = {0: 'green', 1: "blue"}
+capacity = []
+for i in k:
+    capacity.append(1 / i)
+
+
+def classify(k=30, metric="Euclidean", plot=True):
+    if metric == "Euclidean":
+        p = 2
+    elif metric == "Manhattan":
+        p = 1
+    else:
+        return "Invalid Metric"
+    neigh = KNeighborsClassifier(n_neighbors=k, p=p)
+    neigh.fit(train_df[['x1', 'x2']], train_df['label'])
+    if plot:
+        grid_points_copy = grid_points.copy()
+        grid_points_copy['predict'] = neigh.predict(grid_points)
+        _, ax = plt.subplots(figsize=(10, 10))
+        ax.scatter(grid_points_copy['x1'],
+                   grid_points_copy['x2'],
+                   c=grid_points_copy['predict'].map(colors),
+                   marker='.')
+        ax.set_xlabel("$x_1$")
+        ax.set_ylabel("$x_2$")
+        ax.set_title(metric + ", k=" + str(k))
+        plt.savefig('classify.pdf', format='pdf')
+        plt.show()
+    return neigh
+
+
+def draw_plot(neigh, k, metric="Euclidean"):
+    train_err = round(
+        1 - neigh.score(train_df[['x1', 'x2']], train_df['label']), 3)
+    test_err = round(1 - neigh.score(test_df[['x1', 'x2']], test_df['label']),
+                     3)
+    grid_points_copy = grid_points.copy()
+    grid_points_copy['predict'] = neigh.predict(grid_points)
+    _, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(grid_points_copy['x1'],
+               grid_points_copy['x2'],
+               c=grid_points_copy['predict'].map(colors),
+               marker='.')
+    ax.scatter(train_df['x1'],
+               train_df['x2'],
+               c=train_df['label'].map(colors),
+               marker='o')
+    ax.scatter(test_df['x1'],
+               test_df['x2'],
+               c=test_df['label'].map(colors),
+               marker='+')
+    ax.set_xlabel("$x_1$")
+    ax.set_ylabel("$x_2$")
+    ax.set_title(metric + ", k=" + str(k) + ", Training Error Rate: " +
+                 str(train_err) + ", Testing Error Rate: " + str(test_err))
+
+
+def Q1_results():
+    for i in k:
+        neigh = classify(k=i, plot=False)
+        draw_plot(neigh, i)
+        plt.savefig('Q1_' + str(i) + '.pdf', format='pdf')
+        plt.show()
+
+
+def Q2_results():
+    neigh = classify(k=30, metric="Manhattan", plot=False)
+    draw_plot(neigh, 30, "Manhattan")
+    plt.savefig('Q2.pdf', format='pdf')
+    plt.show()
+
+
+def Q3_results():
+    train_list = []
+    test_list = []
+    for i in k:
+        neigh = classify(k=i, plot=False)
+        train_err = round(
+            1 - neigh.score(train_df[['x1', 'x2']], train_df['label']), 3)
+        test_err = round(
+            1 - neigh.score(test_df[['x1', 'x2']], test_df['label']), 3)
+        train_list.append(train_err)
+        test_list.append(test_err)
+    _, err_plot = plt.subplots(figsize=(10, 10))
+    err_plot.plot(capacity, train_list, 'o-r', color='blue', label='Training')
+    err_plot.plot(capacity, test_list, 'o-r', color='red', label='Testing')
+    err_plot.set_xscale("log")
+    err_plot.legend()
+    err_plot.set_title("Euclidean, Error rate versus Model capacity")
+    plt.savefig('Q3.pdf', format='pdf')
+    plt.show()
+
+
+if __name__ == "__main__":
+    Q1_results()
+    Q2_results()
+    Q3_results()
