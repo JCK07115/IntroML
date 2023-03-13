6f84e2849aad30824510e754e48f57c2593ff3f9
Chaoyue Xi <chaoyuex@mun.ca>
2023-03-13 15:04:56 -0230

    Add header

--- assign2/data/assign_2_group_07.py
+++ assign2/data/assign_2_group_07.py
@@ -1,3 +1,12 @@
+"""
+# Assignment 2
+# Group 07
+# ----------
+# Chaoyue Xi                          : chaoyuex@mun.ca
+# Mohammad Hamza Khan                 : mohammadhk@mun.ca
+# Oluwafunmiwo Judah Sholola          : ojsholola@mun.ca
+"""
+
 import pandas as pd
 from sklearn.linear_model import LinearRegression
 
@@ -21,16 +30,23 @@ test_df = pd.read_csv('./test.csv')
 test_df.rename(columns=column_names, inplace=True)
 
 
-def classify(train, test, model="Simple", alpha=0):
+def classify(train, test, model="Simple", k=1, alpha=0):
     if model == "Simple":
-        reg = LinearRegression().fit(train.iloc[:, :8],
-                                     train['Concrete Compressive Strength'])
-        return reg.score(test.iloc[:, :8],
-                         test['Concrete Compressive Strength'])
+        return simpleLR_validation(train, test)
     if model == "Ridge":
         return "TODO"
     if model == "Lasso":
         return "TODO"
+
+
+def simpleLR_validation(train, test):
+    reg = LinearRegression().fit(train.iloc[:, :8],
+                                 train['Concrete Compressive Strength'])
+    return reg.score(test.iloc[:, :8], test['Concrete Compressive Strength'])
+
+
+def simpleLR_CV(train, test, k):
+    pass
 
 
 def Q1_results():
@@ -53,3 +69,4 @@ if __name__ == "__main__":
     Q1_results()
     Q2_results()
     Q3_results()
+    print(classify(train_df, test_df))
