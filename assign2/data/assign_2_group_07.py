99941290fc4763f0c79aa9ef1aff288091d91b25
Oluwafunmiwo Judah Sholola <oluwafunmiwosholola@gmail.com>
2023-03-27 16:22:32 -0230

    q1 complete; added model assessment to validation - using model with optimum hp (from train/validation subsets) on test subset (of training data); refactored cross-validation and added model assessment on actual test dataset

--- assign2/data/assign_2_group_07.py
+++ assign2/data/assign_2_group_07.py
@@ -29,9 +29,9 @@ column_names = {
 }
 
 # creating the training and testing dataframes
-train_df = pd.read_csv('./train.csv')
+train_df = pd.read_csv('train.csv')
 train_df.rename(columns=column_names, inplace=True)
-test_df = pd.read_csv('./test.csv')
+test_df = pd.read_csv('test.csv')
 test_df.rename(columns=column_names, inplace=True)
 
 X_train = train_df.drop(columns=['CCStrength'])
@@ -40,27 +40,65 @@ X_test = test_df.drop(columns=['CCStrength'])
 y_test = test_df['CCStrength']
 
 
-def classify(model, K, tt_rs, tv_rs, train, alpha=0):
-    reg = None
-    if model == "Simple":
-        reg = LinearRegression()
-        print("Simple Linear Regression - [K={K}]")
-    elif model == "Ridge":
-        reg = Ridge(alpha=alpha)
-        print(f"Ridge Regression - [K={K}, alpha={alpha}]")
-    elif model == "Lasso":
-        reg = Lasso(alpha=alpha)
-        print(f"Lasso Regression - [K={K}, alpha={alpha}]")
+def classify(type, tt_rs=None, tv_rs=None, K_set=None, alpha=0):
+    model = None
+    if type == "Simple":
+        model = LinearRegression()
+        if K_set==None:
+            print("Simple Linear Regression")
+    elif type == "Ridge":
+        model = Ridge(alpha=alpha)
+        # print(f"Ridge Regression : [K={K}, alpha={alpha}]")
+    elif type == "Lasso":
+        model = Lasso(alpha=alpha)
+        # print(f"Lasso Regression : [K={K}, alpha={alpha}]")
 
-    if reg:
+    if model and tt_rs!=None:       # validation approach
         val_models = []
-        val_models_scores = []
+        val_models_rse = []
+        val_models_rsq = []
+        val_models_config = []
+        
+        # model selection
         for tt_r in tt_rs:  # train_test_ratio
             for tv_r in tv_rs:  # test_val_ratio
-                mod, score = validation(model, train, tt_r, tv_r)
+                model, score = validation(model, tt_r, tv_r)
+                val_models.append(model)
+                val_models_rse.append(score[0])
+                val_models_rsq.append(score[1])
+                val_models_config.append([tt_r, tv_r])
+                print("-----------------------------------------")
 
-        CV(reg, train, K)
+        # model assessment
+        opt_per_ind = np.argmin(val_models_rse)              # could also use argmax(val_models_rsq) to find optimal model
+        opt_model = val_models[opt_per_ind]
+        opt_tt_r = val_models_config[opt_per_ind][0]
+        opt_tv_r = val_models_config[opt_per_ind][1]
+        validation(opt_model, opt_tt_r, opt_tv_r, test=True)
+
         print("=========================================")
+    elif model and K_set!=None:     # cross-validation approach
+        opt_model = None
+        min_RSE = 10000000
+
+        # model selection
+        for K in K_set:
+            print(f"{type} Linear Regression : [K={K}]")
+
+            model, rse = CV(model, K)
+            if rse < min_RSE:
+                min_RSE = rse
+                opt_model = model
+
+            print("=========================================")
+        
+        # model assessment
+        opt_model.fit(X_train, y_train)
+        rse = RSE(opt_model, X_test, y_test)
+        rsquare = RSq(opt_model, X_test, y_test)
+        print(f"RSE_cv_test: {rse}")
+        print(f"RSquare_cv_test: {rsquare}")
+
     else:
         print("Unknown Error")
 
@@ -75,67 +113,91 @@ def RSE(model, x_test, y_true):
     return math.sqrt(RSS / (len(y_true) - 2))
 
 
-def validation(model, train, tt_r, tv_r):
+def validation(model, tt_r, tv_r, test=False):
     # train is now {tt_r} of the entire data set
-    x_train, x_test, y_train, y_test = train_test_split(X_train,
+    x_train_sub, x_test_sub, y_train_sub, y_test_sub = train_test_split(X_train,
                                                         y_train,
                                                         test_size=1 - tt_r)
-
+    
     # test is now {1-tt_r-(tv_r*0.5)} of the initial data set
     # validation is now {1-tt_r-(tv_r*0.5)} of the initial data set
-    x_val, x_test, y_val, y_test = train_test_split(x_test,
-                                                    y_test,
+    x_val_sub, x_test_sub, y_val_sub, y_test_sub = train_test_split(x_test_sub,
+                                                    y_test_sub,
                                                     test_size=tv_r)
+    
+    if not test:
+        print(f"x_train_sub: {len(x_train_sub)}, y_train_sub: {len(y_train_sub)}")
+        print(f"x_val_sub: {len(x_val_sub)}, y_val_sub: {len(y_val_sub)}")
+        # print(f"x_test_sub: {len(x_test_sub)}, y_test_sub: {len(y_test_sub)}\n")
 
-    # print(len(x_train), len(x_val), len(x_test))
+        model.fit(x_train_sub, y_train_sub)
+        rse = RSE(model, x_val_sub, y_val_sub)
+        rsquare = RSq(model, x_val_sub, y_val_sub)
+    
+        print("Validation:")
+        print(f"RSE_val_sub: {rse}")
+        print(f"RSquare_val_sub: {rsquare}")
+        return model, [rse, rsquare]
+    else:
+        x_train_sub = pd.concat([x_train_sub, x_val_sub], axis=0)
+        y_train_sub = pd.concat([y_train_sub, y_val_sub], axis=0)
 
-    model.fit(x_train, y_train)
-    rse = RSE(model, x_val, y_val)
-    rsquare = RSq(model, x_val, y_val)
-    print("Validation:")
-    print(f"RSE: {rse}")
-    print(f"R^2: {rsquare}")
-    return model
+        print(f"x_train_sub: {len(x_train_sub)}, y_train_sub: {len(y_train_sub)}")
+        print(f"x_test_sub: {len(x_test_sub)}, y_test_sub: {len(y_test_sub)}\n")
 
+        model.fit(x_train_sub, y_train_sub)
+        rse = RSE(model, x_test_sub, y_test_sub)
+        rsquare = RSq(model, x_test_sub, y_test_sub)
+    
+        print("Test:")
+        print(f"RSE_test_sub: {rse}")
+        print(f"RSquare_test_sub: {rsquare}")
+        return model, [rse, rsquare]
 
-def CV(model, K, train):
+def CV(model, K):
     kf = KFold(n_splits=K, shuffle=True)
-    # X = train.iloc[:, :8]
-    # y = train['Concrete Compressive Strength']
     RSq_vals = []
     RSE_vals = []
+    min_RSE = 1000000000
+    opt_model = None
 
+    # model selection
     for _, (train_index, test_index) in enumerate(kf.split(X_train, y_train)):
         X_train_folds = X_train.iloc[train_index]
         y_train_folds = y_train.iloc[train_index]
         X_test_folds = X_train.iloc[test_index]
         y_test_folds = y_train.iloc[test_index]
-        # reg = model.fit(X_train_folds, y_train_folds)
         model.fit(X_train_folds, y_train_folds)
-        RSq_vals += [RSq(model, X_test_folds, y_test_folds)]
-        RSE_vals += [RSE(model, X_test_folds, y_test_folds)]
+        rse = RSE(model, X_test_folds, y_test_folds)
+        rsquare = RSq(model, X_test_folds, y_test_folds)
+        RSE_vals.append(rse)
+        RSq_vals.append(rsquare)
+
+        if rse < min_RSE:
+            min_RSE = rse
+            opt_model = model
 
     print(f"Cross Validation(n_splits={K}):")
-    print(f"RSE: {np.mean(RSq_vals)}")
-    print(f"R^2: {np.mean(RSE_vals)}")
+    print(f"RSE_cv_fold: {np.mean(RSE_vals)}")
+    print(f"RSquare_cv_fold: {np.mean(RSq_vals)}")
+
+    return opt_model, min_RSE 
+
 
 
 def Q1_results():
-    # validation (alpha's are ratio of )
+    # validation (alpha's are ratio of training subset to test subset)
     train_test_ratios = [0.2, 0.3, 0.5]
-    test_val_ratios = [
-        0.5
-    ]  # setting validation and test to be exactly half of whatever remains from tt_r split (10%, 15%, 25% ea.)
+    test_val_ratios = [0.5]  # setting validation and test subsets to be exactly half of whatever remains from tt_r split (10%, 15%, 25% ea.)
 
-    classify(model="Simple",
+    classify(type="Simple",
              tt_rs=train_test_ratios,
-             tv_rs=test_val_ratios,
-             train=train_df)
+             tv_rs=test_val_ratios)
 
     # cross-validation
     K_set = [2, 3, 5, 10, 15, 20, 30, 50, 100]
-    for K in K_set:
-        classify(model="Simple", K=K, train=train_df)
+    classify(type="Simple", 
+             K_set=K_set)
 
 
 def Q2_results():
@@ -148,13 +210,13 @@ def Q2_results():
 
     # Train and test model with best alpha
     alpha = clf.best_params_['alpha']
-    reg = Ridge(alpha)
-    reg.fit(X_train, y_train)
-    R_square = r2_score(y_test, reg.predict(X_test))
-    rse = RSE(reg, X_test, y_test)
+    model = Ridge(alpha)
+    model.fit(X_train, y_train)
+    rse = RSE(model, X_test, y_test)
+    rsquare = RSq(model, X_test, y_test)
     print(f"Ridge Regression - alpha={alpha}")
     print(f"RSE: {rse}")
-    print(f"R^2: {R_square}")
+    print(f"RSquare: {rsquare}")
     print("=========================================")
 
     # Generate plot
@@ -177,13 +239,13 @@ def Q3_results():
 
     # Train and test model with best alpha
     alpha = clf.best_params_['alpha']
-    reg = Lasso(alpha)
-    reg.fit(X_train, y_train)
-    R_square = r2_score(y_test, reg.predict(X_test))
-    rse = RSE(reg, X_test, y_test)
+    model = Lasso(alpha)
+    model.fit(X_train, y_train)
+    rse = RSE(model, X_test, y_test)
+    rsquare = RSq(model, X_test, y_test)
     print(f"Lasso Regression - alpha={alpha}")
     print(f"RSE: {rse}")
-    print(f"R^2: {R_square}")
+    print(f"RSquare: {rsquare}")
     print("=========================================")
 
     # Generate plot
@@ -210,15 +272,16 @@ def predictCompressiveStrength(Xtest, data_dir):
     # Model: Polynomial regression with degree=3
     poly = PolynomialFeatures(degree=3, include_bias=False)
     train_poly = poly.fit_transform(Xtrain, ytrain)
-    reg = LinearRegression()
-    reg.fit(train_poly, ytrain)
+    model = LinearRegression()
+    model.fit(train_poly, ytrain)
 
     # Predict the test value
     test_poly = poly.fit_transform(Xtest)
-    return reg.predict(test_poly)
+    return model.predict(test_poly)
 
 
 if __name__ == "__main__":
-    # Q1_results()
-    Q2_results()
-    Q3_results()
+    Q1_results()
+    # Q2_results()
+    # Q3_results()
+    # predictCompressiveStrength()
