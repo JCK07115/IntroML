e754ce17ff55a794f9e6b65b21ac37156a7982a1
Chaoyue Xi <chaoyuex@mun.ca>
2023-04-15 19:04:52 -0230

    Refactor the codebase

--- project/project_group_07.py
+++ project/project_group_07.py
@@ -1,42 +1,71 @@
+import os.path
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 import tensorflow as tf
-import os.path
 from pathlib import Path
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+from tensorflow.keras import layers
+from sklearn.metrics import confusion_matrix, roc_auc_score
+from keras.models import model_from_json
 
-image_dir = Path('../input/age-prediction/20-50/20-50')
-sample_num = 5000
+train_num = 4500
+test_num = 500
 random_state = 1
-train_size = 0.7
+labels = ['20-24', '25-29', '30-34', '35-49', '40-44', '45-50']
 
-# Dataframes
-filepaths = pd.Series(list(image_dir.glob(r'**/*.jpg')),
-                      name='Filepath').astype(str)
-ages = pd.Series(
-    filepaths.apply(lambda x: os.path.split(os.path.split(x)[0])[1]),
-    name='Age').astype(int)
-images = pd.concat([filepaths, ages], axis=1).sample(
-    frac=1.0, random_state=random_state).reset_index(drop=True)
-image_df = images.sample(sample_num,
-                         random_state=random_state).reset_index(drop=True)
-train_df, test_df = train_test_split(image_df,
-                                     train_size=train_size,
-                                     shuffle=True,
-                                     random_state=random_state)
+# Data preparation
+image_dir_test = Path('../input/age-prediction/20-50/20-50/test')
+image_dir_train = Path('../input/age-prediction/20-50/20-50/train')
+
+
+def preprocessing(image_dir):
+    filepaths = pd.Series(list(image_dir.glob(r'**/*.jpg')),
+                          name='Filepath').astype(str)
+    ages = filepaths.apply(
+        lambda x: os.path.split(os.path.split(x)[0])[1]).astype(np.int16)
+    ranged_ages = []
+    for age in ages:
+        if 20 <= age <= 24:
+            ranged_ages.append('20-24')
+        elif 25 <= age <= 29:
+            ranged_ages.append('25-29')
+        elif 30 <= age <= 34:
+            ranged_ages.append('30-34')
+        elif 35 <= age <= 39:
+            ranged_ages.append('35-49')
+        elif 40 <= age <= 44:
+            ranged_ages.append('40-44')
+        elif 45 <= age <= 50:
+            ranged_ages.append('45-50')
+    s_ages = pd.Series(ranged_ages, name='Age').astype(str)
+    images = pd.concat([filepaths, s_ages],
+                       axis=1).sample(frac=1).reset_index(drop=True)
+    return images
+
+
+train_df = preprocessing(image_dir_train).sample(
+    train_num, random_state=random_state).reset_index(drop=True)
+test_df = preprocessing(image_dir_test).sample(
+    test_num, random_state=random_state).reset_index(drop=True)
 
 # Loading images
 train_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-    rescale=1. / 255, validation_split=0.2)
-test_generator = tf.keras.preprocessing.image.ImageDataGenerator(rescale=1. /
-                                                                 255)
+    rescale=1. / 255,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    validation_split=0.2)
+test_generator = tf.keras.preprocessing.image.ImageDataGenerator(
+    rescale=1. / 255,
+    horizontal_flip=True,
+    zoom_range=0.2,
+)
 train_images = train_generator.flow_from_dataframe(dataframe=train_df,
                                                    x_col='Filepath',
                                                    y_col='Age',
                                                    target_size=(120, 120),
                                                    color_mode='rgb',
-                                                   class_mode='raw',
+                                                   class_mode='categorical',
                                                    batch_size=32,
                                                    shuffle=True,
                                                    seed=42,
@@ -46,7 +75,7 @@ val_images = train_generator.flow_from_dataframe(dataframe=train_df,
                                                  y_col='Age',
                                                  target_size=(120, 120),
                                                  color_mode='rgb',
-                                                 class_mode='raw',
+                                                 class_mode='categorical',
                                                  batch_size=32,
                                                  shuffle=True,
                                                  seed=42,
@@ -56,43 +85,136 @@ test_images = test_generator.flow_from_dataframe(dataframe=test_df,
                                                  y_col='Age',
                                                  target_size=(120, 120),
                                                  color_mode='rgb',
-                                                 class_mode='raw',
+                                                 class_mode='categorical',
                                                  batch_size=32,
                                                  shuffle=False)
 
-# Train
-inputs = tf.keras.Input(shape=(120, 120, 3))
-x = tf.keras.layers.Conv2D(filters=16, kernel_size=(3, 3),
-                           activation='relu')(inputs)
-x = tf.keras.layers.MaxPool2D()(x)
-x = tf.keras.layers.Conv2D(filters=32, kernel_size=(3, 3),
-                           activation='relu')(x)
-x = tf.keras.layers.MaxPool2D()(x)
-x = tf.keras.layers.GlobalAveragePooling2D()(x)
-x = tf.keras.layers.Dense(64, activation='relu')(x)
-x = tf.keras.layers.Dense(64, activation='relu')(x)
-outputs = tf.keras.layers.Dense(1, activation='linear')(x)
 
-model = tf.keras.Model(inputs=inputs, outputs=outputs)
+# Functions
+def compile_model(model):
+    opt = tf.keras.optimizers.Adam(learning_rate=0.0003)
+    model.compile(optimizer=opt,
+                  loss='categorical_crossentropy',
+                  metrics=['acc'])
+    print(model.name, 'compiled')
 
-model.compile(optimizer='adam', loss='mse')
 
-history = model.fit(train_images,
-                    validation_data=val_images,
-                    epochs=100,
-                    callbacks=[
-                        tf.keras.callbacks.EarlyStopping(
-                            monitor='val_loss',
-                            patience=5,
-                            restore_best_weights=True)
-                    ])
+def fit_model(model):
+    history = model.fit(train_images,
+                        validation_data=val_images,
+                        epochs=20,
+                        callbacks=[
+                            tf.keras.callbacks.EarlyStopping(
+                                monitor='val_loss',
+                                patience=5,
+                                restore_best_weights=True)
+                        ])
+    print(model.name, 'fitted')
+    return history
 
-# Test
-predicted_ages = np.squeeze(model.predict(test_images))
-true_ages = test_images.labels
 
-rmse = np.sqrt(model.evaluate(test_images, verbose=0))
-print("     Test RMSE: {:.5f}".format(rmse))
+def plot_history(history):
+    history_frame = pd.DataFrame(history.history)
+    history_frame.loc[:, ['loss', 'val_loss']].plot()
 
-r2 = r2_score(true_ages, predicted_ages)
-print("Test R^2 Score: {:.5f}".format(r2))
+
+def evaluate_model(model):
+    predicted_emotions_squeezed = np.squeeze(model.predict(test_images))
+    true_emotions = test_images.labels
+    predicted_emotions = []
+    for pred in predicted_emotions_squeezed:
+        predicted_emotions.append(np.argmax(pred))
+    roc = roc_auc_score(true_emotions,
+                        predicted_emotions_squeezed,
+                        multi_class='ovr')
+    print("Test ROC Score: {:.5f}".format(roc))
+    plot_cf_matrix(true_emotions, predicted_emotions)
+
+
+def plot_cf_matrix(true_emotions, predicted_emotions):
+    cf_mat = confusion_matrix(true_emotions, predicted_emotions)
+    plt.figure(figsize=(10, 10))
+    ax = sns.heatmap(cf_mat, annot=True, cmap='Blues', fmt="d")
+    ax.set_title('Confusion Matrix\n\n')
+    ax.set_xlabel('\nPredicted Values')
+    ax.set_ylabel('Actual Values ')
+    ax.xaxis.set_ticklabels(labels)
+    ax.yaxis.set_ticklabels(labels)
+    plt.show()
+
+
+def train_model(model):
+    compile_model(model)
+    history = fit_model(model)
+    plot_history(history)
+    evaluate_model(model)
+
+
+def save_model(model):
+    model_json = model.to_json()
+    with open(model.name + ".json", "w") as json_file:
+        json_file.write(model_json)
+    model.save_weights(model.name + ".h5")
+    print(model.name + " is saved")
+
+
+def load_model(model):
+    json_file = open(model.name + ".json", "r")
+    loaded_model_json = json_file.read()
+    json_file.close()
+    loaded_model = model_from_json(loaded_model_json)
+    loaded_model.load_weights(model.name + ".h5")
+    print(model.name + " is loaded")
+    return loaded_model
+
+
+# Model
+model = tf.keras.Sequential(
+    [
+        layers.InputLayer(input_shape=[120, 120, 3]),
+
+        # Block One
+        layers.Conv2D(filters=64, kernel_size=3, strides=2, padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.MaxPool2D(strides=2),
+        layers.Dropout(0.187),
+
+        # Block Two
+        layers.Conv2D(filters=128, kernel_size=3, padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.MaxPool2D(strides=2),
+        layers.Dropout(0.187),
+
+        # Block Three
+        layers.Conv2D(filters=256, kernel_size=3, padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.MaxPool2D(strides=2),
+        layers.Dropout(0.187),
+
+        # Block Four
+        layers.Conv2D(filters=512, kernel_size=3, padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.MaxPool2D(strides=2),
+        layers.Dropout(0.187),
+
+        # Block Five
+        layers.Conv2D(filters=1024, kernel_size=3, padding='same'),
+        layers.BatchNormalization(),
+        layers.Activation('relu'),
+        layers.Dropout(0.187),
+
+        # Head
+        layers.Flatten(),
+        layers.Dense(256, activation='relu'),
+        layers.Dense(128, activation='relu'),
+        layers.Dense(6, activation='softmax')
+    ],
+    name='model')
+
+# Train and test
+train_model(model)
+save_model(model)
